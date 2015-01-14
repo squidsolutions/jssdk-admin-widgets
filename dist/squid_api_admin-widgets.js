3362cb044b24b8ab4d77d550f147f048e7b5ce49
obalbous <oba@squid>
2015-01-14 09:31:35 +0100

    synced dist

--- dist/squid_api_admin-widgets.js
+++ dist/squid_api_admin-widgets.js
@@ -4,43 +4,13 @@ this["squid_api"]["template"] = this["squid_api"]["template"] || {};
 this["squid_api"]["template"]["squid_api_users_admin_widget"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
-  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression, self=this;
+  
 
-function program1(depth0,data) {
-  
-  var buffer = "", stack1, helper;
-  buffer += "\n<tr>\n    <td>";
-  if (helper = helpers.login) { stack1 = helper.call(depth0, {hash:{},data:data}); }
-  else { helper = (depth0 && depth0.login); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
-  buffer += escapeExpression(stack1)
-    + "</td> \n    <td>";
-  if (helper = helpers.name) { stack1 = helper.call(depth0, {hash:{},data:data}); }
-  else { helper = (depth0 && depth0.name); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
-  buffer += escapeExpression(stack1)
-    + "</td>\n    <td>";
-  if (helper = helpers.email) { stack1 = helper.call(depth0, {hash:{},data:data}); }
-  else { helper = (depth0 && depth0.email); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
-  buffer += escapeExpression(stack1)
-    + "</td>\n    <td>";
-  stack1 = helpers.each.call(depth0, (depth0 && depth0.groups), {hash:{},inverse:self.noop,fn:self.program(2, program2, data),data:data});
-  if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "</td>\n</tr>\n";
-  return buffer;
-  }
-function program2(depth0,data) {
-  
-  
-  return escapeExpression((typeof depth0 === functionType ? depth0.apply(depth0) : depth0));
-  }
 
-  buffer += "<table>\n";
-  stack1 = helpers.each.call(depth0, (depth0 && depth0.users), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
-  if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "\n</table>";
-  return buffer;
+  return "<div class='sq-loading' style='position:absolute; width:100%; top:40%; z-index: 1;'>\n    <div class=\"spinner\">\n    <div class=\"rect5\"></div>\n    <div class=\"rect4\"></div>\n    <div class=\"rect3\"></div>\n    <div class=\"rect2\"></div>\n    <div class=\"rect1\"></div>\n    <div class=\"rect2\"></div>\n    <div class=\"rect3\"></div>\n    <div class=\"rect4\"></div>\n    <div class=\"rect5\"></div>\n    </div>\n</div>\n<table id=\"squid-api-admin-widgets-user-table\" class=\"sq-table\">\n    <thead>\n        <tr>\n            <th>Login</th>\n            <th>Email</th>\n            <th>Name</th>\n            <th>Groups</th>\n            <th>Action</th>\n        </tr>\n    </thead>\n    <tbody>\n        <tr>\n            <td></td>\n            <td></td>\n            <td></td>\n            <td></td>\n            <td></td>\n        </tr>\n    </tbody>\n</table>";
   });
 (function (root, factory) {
-    root.squid_api.view.UsersAdmin = factory(root.Backbone, root.squid_api, squid_api.template.squid_api_users_admin_widget);
+    root.squid_api.view.UsersAdminView = factory(root.Backbone, root.squid_api, squid_api.template.squid_api_usersadmin_widget);
 
 }(this, function (Backbone, squid_api, template) {
 
@@ -54,9 +24,9 @@ function program2(depth0,data) {
             if (options.template) {
                 this.template = options.template;
             } else {
-                this.template = template;
+                this.template = squid_api.template.squid_api_users_admin_widget;
             }
-
+            
             // init the model
             if (!this.model) {
                 var users = new squid_api.model.UserCollection({"id" : {"customerId" : squid_api.customerId}});
@@ -76,35 +46,71 @@ function program2(depth0,data) {
                     }
                 });
             }
-            this.model.on("reset sync", this.process, this);
-            
-        },
 
-        events: {
-        },
-        
-        process : function() {
-            this.render();
+            this.model.on("reset sync", this.render, this);
         },
 
         render: function() {
-            // display
+            var me = this;
+            this.$el.html(this.template());
 
-            var user, jsonData = {"users" : []};
+            var globalID;
+            var users = this.model.models;
 
-            for (var i=0; i<this.model.size(); i++) {
-                user = this.model.at(i);
-                if (user) {
-                    // add to display
-                    jsonData.users.push(user.toJSON());
-                }
+            if (this.$el.attr("id")) {
+                globalID = "#" + this.$el.attr('id');
+            } else {
+                console.log("No ID assigned to DOM element for User Table");
             }
 
-            var html = this.template(jsonData);
-            this.$el.html(html);
-            this.$el.show();
+            if (users) {
+                var tableRows = d3.select(globalID + " tbody").selectAll("tr")
+                    .data(users).enter().append("tr");
 
-            return this;
+                var loginValue = tableRows.append("td")
+                    .text(function(d) {
+                        return d.get("objectType");
+                    });
+
+                var emailValue = tableRows.append("td")
+                    .text(function(d) {
+                        return d.get("email");
+                    });
+
+                var nameValue = tableRows.append("td")
+                    .text(function(d) {
+                        return d.get("login");
+                    });
+
+                var groupValues = tableRows.append("td")
+                    .html(function(d) {
+                        var g = d.get("groups");
+                        var data = "";
+                        for (i=0; i<g.length; i++) {
+                            if (g[i] === "admin") {
+                                data += "<div class='red'>" + g[i] + "</div>";
+                            } else {
+                                var pattern = /admin_/;
+                                if (pattern.test(g[i])) {
+                                    data += "<div class='orange'>" + g[i] + "</div>";
+                                } else {
+                                    data += "<div>" + g[i] + "</div>";
+                                }
+                            }    
+                        }
+                        return data;
+                    });
+
+                var actionValues = tableRows.append("td")
+                    .html(function(d) {
+                        return "";
+                    });
+            }
+
+            this.$el.find("#squid-api-admin-widgets-user-table").DataTable({
+                "lengthChange": false
+            });
+
         }
 
     });
