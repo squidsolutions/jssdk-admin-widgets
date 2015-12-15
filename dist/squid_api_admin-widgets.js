4e65120d322379cb5fe96abc5c65f288102d31cf
Thomas Scott <thomas.scott@squidsolutions.com>
2015-12-15 21:19:09 +0100

    override domain collection management view to

--- dist/squid_api_admin-widgets.js
+++ dist/squid_api_admin-widgets.js
@@ -1931,11 +1931,46 @@ function program1(depth0,data) {
 
             // listen for project change
             this.config.on("change:project", function (config) {
+                // add before fetch
                 squid_api.getSelectedProjectCollection("domains").done( function(domains) {
                     me.collection = domains;
                     me.initListeners();
                 });
             });
+        },
+        render: function() {
+            console.log("render CollectionManagementWidget "+this.type);
+            // store models
+            if (this.collection) {
+                var jsonData = {
+                    models : [],
+                    roles : this.getRoles(),
+                    typeLabelPlural : this.typeLabelPlural,
+                    modalHtml : true
+                };
+                for (i=0; i<this.collection.size(); i++) {
+                    var model = {};
+                    model.label = this.collection.at(i).get("name");
+                    model.value = this.collection.at(i).get("oid");
+                    model.roles = this.getRoles();
+
+                    if (this.collection.at(i).get("dynamic")) {
+                        model.label = "~ " + model.label;
+                    }
+
+                    // detect selected model
+                    if (model.value === this.config.get(this.type.toLowerCase())) {
+                        model.selected = true;
+                    }
+                    jsonData.models.push(model);
+                }
+
+                // print template
+                var html = this.template(jsonData);
+                this.$el.html(html);
+            }
+
+            return this;
         }
 
     });
