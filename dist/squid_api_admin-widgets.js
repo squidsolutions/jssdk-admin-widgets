60da9fbb25510f907ff79d928b815d73a57d134c
Thomas Scott <thomas.scott@squidsolutions.com>
2016-01-07 10:35:02 +0100

    add project creator button to detail roles

--- dist/squid_api_admin-widgets.js
+++ dist/squid_api_admin-widgets.js
@@ -3192,6 +3192,38 @@ function program1(depth0,data) {
     return View;
 }));
 
+(function (root, factory) {
+    root.squid_api.view.ProjectCreatorButton = factory(root.Backbone, root.squid_api, squid_api.template.squid_api_button_view);
+
+}(this, function (Backbone, squid_api, template) {
+
+    var View = squid_api.view.ProjectCollectionManagementWidget.extend({
+
+        init : function() {
+            var me = this;
+            this.listenTo(this.config,"change", this.configCompare);
+        },
+
+        render: function() {
+            var label = this.typeLabelPlural;
+            var jsonData = {
+                label : "Create a new one",
+                visible : this.getCreateRole(),
+                collectionLoaded : !this.collectionLoading,
+                collection : this.collection,
+                typeLabelPlural : this.typeLabelPlural
+            };
+
+            this.$el.html(template(jsonData));
+
+            return this;
+        }
+
+    });
+
+    return View;
+}));
+
 
 (function (root, factory) {
     root.squid_api.view.ProjectModelManagementWidget = factory(root.Backbone, root.squid_api);
